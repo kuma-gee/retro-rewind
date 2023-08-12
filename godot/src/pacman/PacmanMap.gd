355708525566399025185719df04515cc257591b
myin <minyin142p@gmail.com>
2023-08-12 17:41:30 +0200

    move ghost

--- godot/src/pacman/PacmanMap.gd
+++ godot/src/pacman/PacmanMap.gd
@@ -1,26 +1,26 @@
 extends TileMap
 
-func do_move(node: Node2D, motion: Vector2i, finish_fn: Callable):
+func do_move(node: Node2D, motion: Vector2i, finish_fn: Callable, speed = 0.2):
 	var pos = local_to_map(node.position)
 	if motion.x != 0:
 		var dest = pos + Vector2i(motion.x, 0)
 		if not _is_wall(dest):
-			return _move(node, dest, finish_fn)
+			return _move(node, dest, finish_fn, speed)
 			
 	if motion.y != 0:
 		var dest = pos + Vector2i(0, motion.y)
 		if not _is_wall(dest):
-			return _move(node, dest, finish_fn)
+			return _move(node, dest, finish_fn, speed)
 
 func _is_wall(p: Vector2i):
 	var cell = get_cell_source_id(0, p)
 	return cell != -1
 
-func _move(node: Node2D, dest: Vector2i, finish_fn: Callable):
+func _move(node: Node2D, dest: Vector2i, finish_fn: Callable, speed = 0.2):
 	var rect = get_used_rect()
 	var pos = map_to_local(dest)
 	var tw = create_tween()
-	tw.tween_property(node, "position", pos, 0.2)
+	tw.tween_property(node, "position", pos, speed)
 	tw.finished.connect(func():
 		if dest.x >= rect.size.x:
 			var start = map_to_local(Vector2i(-1, dest.y))
@@ -28,7 +28,7 @@ func _move(node: Node2D, dest: Vector2i, finish_fn: Callable):
 			
 			var other_pos = map_to_local(Vector2i(0, dest.y))
 			var tw2 = create_tween()
-			tw2.tween_property(node, "position", other_pos, 0.2)
+			tw2.tween_property(node, "position", other_pos, speed)
 			tw2.finished.connect(finish_fn)
 		elif dest.x <= -1:
 			var start = map_to_local(Vector2i(rect.size.x, dest.y))
@@ -36,7 +36,7 @@ func _move(node: Node2D, dest: Vector2i, finish_fn: Callable):
 			
 			var other_pos = map_to_local(Vector2i(rect.size.x - 1, dest.y))
 			var tw2 = create_tween()
-			tw2.tween_property(node, "position", other_pos, 0.2)
+			tw2.tween_property(node, "position", other_pos, speed)
 			tw2.finished.connect(finish_fn)
 		else:
 			finish_fn.call()
--- godot/src/pacman/pacman_ghost.gd
+++ godot/src/pacman/pacman_ghost.gd
@@ -1,3 +1,31 @@
 extends CharacterBody2D
 
 @export var tilemap: TileMap
+
+var moving
+var local_paths = []
+
+func _process(delta):
+	if moving != null:
+		return
+	
+	var player = get_tree().get_first_node_in_group("player")
+	var path = NavigationServer2D.map_get_path(get_world_2d().navigation_map, global_position, player.global_position, false)
+	if path.size() > 1:
+		local_paths = []
+		for p in path:
+			var l = to_local(p)
+			local_paths.append(l)
+			
+		var dir = local_paths[1].normalized()
+		var rounded = Vector2i(round(dir.x), round(dir.y))
+		moving = tilemap.do_move(self, rounded, func(): moving = null, 0.25)
+		queue_redraw()
+
+func _draw():
+	if local_paths.size() > 0:
+		var start = local_paths[0]
+		for i in range(1, local_paths.size()):
+			var end = local_paths[i]
+			draw_line(start, end, Color.RED, 2)
+			start = end
